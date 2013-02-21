e28bfc92b93351138181f099378f00c4af297781
Brandon <bigb@digitalrogues.com>
2013-02-21 09:10:37 -0800

    Initial Commit

---
+++ QSR2/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  QSR2
+//
+//  Created by punk on 2/21/13.
+//  Copyright (c) 2013 Digital Rogues. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ QSR2/AppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.m
+//  QSR2
+//
+//  Created by punk on 2/21/13.
+//  Copyright (c) 2013 Digital Rogues. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+	// Insert code here to initialize your application
+}
+
+@end
